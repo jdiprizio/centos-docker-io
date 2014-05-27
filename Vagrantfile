4c076c044d803c8c6b2f84188f7fe1695c0c47e0
Jérémie Di Prizio <jeremie.diprizio@jemast.com>
2014-05-27 11:20:12 +0200

    Initial commit

---
+++ Vagrantfile
@@ -0,0 +1,7 @@
+# -*- mode: ruby -*-
+# vi: set ft=ruby :
+
+Vagrant.configure("2") do |config|
+  config.vm.box = "centos-docker-io"
+  config.vm.box_url = "centos-docker-io.box"
+end
---
+++ docker.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+set -e
+
+# epel
+curl -o /etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL -L "http://ftp.jaist.ac.jp/pub/Linux/Fedora/epel/RPM-GPG-KEY-EPEL"
+rpm -ivh "http://ftp.jaist.ac.jp/pub/Linux/Fedora/epel/6/x86_64/epel-release-6-8.noarch.rpm"
+
+# docker
+yum install -y docker-io
