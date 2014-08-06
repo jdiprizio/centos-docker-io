c440e0df23df4a656fb934af695d47d83e944674
Jérémie Di Prizio <jeremie.diprizio@jemast.com>
2014-08-06 17:29:51 +0200

    Fixed security parameters for proper Docker operation

--- docker.sh
+++ docker.sh
@@ -2,9 +2,6 @@
 
 set -e
 
-# epel
-#curl -o /etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL -L "http://ftp.jaist.ac.jp/pub/Linux/Fedora/epel/RPM-GPG-KEY-EPEL"
-#rpm -ivh "http://ftp.jaist.ac.jp/pub/Linux/Fedora/epel/6/x86_64/epel-release-6-8.noarch.rpm"
-
 # docker
+usermod -a -G docker vagrant
 yum install -y docker
